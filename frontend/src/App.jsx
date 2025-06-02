3660b7ce3a2fe073c417ebf7d8fe78d0b0caf3a5
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-02 16:03:00 +0900

    各ページのルーティング

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SoloPlay from './pages/SoloPlay';
+import Battle from './pages/Battle';
 
 function App() {
 
   return (
-    <>
-      hello
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/solo" element={<SoloPlay />} />
+        <Route path="/battle" element={<Battle />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
