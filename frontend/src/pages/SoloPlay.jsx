72d4dbd694222a84db5c8cad0a4ab10f017504b9
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-04 11:57:20 +0900

    1人プレイ用各コンポーネントをSoloPlay.jsxに集約

--- frontend/src/pages/SoloPlay.jsx
+++ frontend/src/pages/SoloPlay.jsx
@@ -1,5 +1,26 @@
-function SoloPlay() {
+import { useState } from 'react';
+import { LoadScript } from '@react-google-maps/api';
+import ResultScreen from './ResultScreen.jsx';
+import SoloPlayContainer from "../components/SoloPlayContainer";
 
+const SoloPlay = () => {
+  const [gameState, setGameState] = useState('playing');
+  const [resultData, setResultData] = useState({ distance: null, initialCoords: null, markerPosition: null });
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+  const handleFinish = ({ distance, initialCoords, markerPosition }) => {
+    setResultData({ distance, initialCoords, markerPosition });
+    setGameState('result');
+  };
+
+  return (
+    <>
+      <LoadScript googleMapsApiKey={apiKey}>
+        {gameState === 'playing' &&  <SoloPlayContainer onFinish={handleFinish} />}
+        {gameState === 'result' && <ResultScreen result={resultData} />}
+      </LoadScript>
+    </>
+  );
 }
 
 export default SoloPlay;
