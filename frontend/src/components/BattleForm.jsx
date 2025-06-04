10e7b24fdf4c137bf166d5cc87fafe0864c0d536
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-04 10:53:31 +0900

    Home.jsxのbattle用のフォームをBattleForm.jsxに切り出し

---
+++ frontend/src/components/BattleForm.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useValidation from '../hooks/useValidation';
+import ErrorMessage from './ErrorMessage';
+
+const BattleForm = ({ resetErrorTrigger }) => {
+  const [roomId, setRoomId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const { validateUser } = useValidation();
+
+  useEffect(() => {
+    setError('');
+  }, [resetErrorTrigger]);
+
+  const handleBattleClick = async () => {
+    const validationError = await validateUser(userName, roomId);
+    if (validationError) {
+      setError(validationError);
+      return;
+    }
+    setError('');
+    navigate('/battle', { state: { userName, roomId } });
+  };
+
+  return (
+    <>
+      <ErrorMessage message={error} />
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder='名前'
+      />
+      <br />
+      <input
+        type="text"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+        placeholder='ルームID(4~20文字の半角英数字)'
+      />
+      <br />
+      <button onClick={handleBattleClick}>ゲーム開始</button>
+    </>
+  );
+}
+
+export default BattleForm;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,20 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useValidation from '../hooks/useValidation';
-import ErrorMessage from '../components/ErrorMessage';
+import BattleForm from '../components/BattleForm';
 
 const Home = () => {
   const [gameState, setGameState] = useState('solo');
-  const [roomId, setRoomId] = useState('');
-  const [userName, setUserName] = useState('');
-  const [error, setError] = useState('');
+  const [resetErrorTrigger, setResetErrorTriger] = useState(false);
   const navigate = useNavigate();
-  const { validateUser } = useValidation();
-
-  const handleBattleClick = async () => {
-    const validationError = await validateUser(userName, roomId);
-    if (validationError) {
-      setError(validationError);
-      return;
-    }
-    setError('');
-    navigate('/battle', { state: { userName, roomId } });
-  };
 
   return (
     <div style={{ padding: 20 }}>
       <h2>Home</h2>
       <button
-        onClick={() => (setGameState('solo'), setError(''))}
+        onClick={() => {
+          setGameState('solo');
+          setResetErrorTriger(prev => !prev);
+        }}
         disabled={gameState === 'solo'}
       >
         1人プレイ
@@ -44,26 +33,7 @@ const Home = () => {
       ) : (
         <>
           <h2>1対1</h2>
-          <ErrorMessage message={error} />
-          <input
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='名前'
-          />
-          <br />
-          <input
-            type="text"
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-            placeholder='ルームID(4~20文字の半角英数字)'
-          />
-          <br />
-          <button
-            onClick={handleBattleClick}
-          >
-            ゲーム開始
-          </button>
+          <BattleForm resetErrorTrigger={resetErrorTrigger} />
         </>
       )}
     </div>
