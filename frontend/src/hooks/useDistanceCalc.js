079c6b112e3ed4e151e5d8932334eb7d1e6664dc
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-04 11:34:57 +0900

    useDistanceCalc.js - 距離計算用hook作成

---
+++ frontend/src/hooks/useDistanceCalc.js
@@ -0,0 +1,26 @@
+export const useDistanceCalc = () => {
+  const toRad = (degrees) => {
+    return degrees * Math.PI / 180;
+  };
+
+  const distanceCalc = (initialCoords, markerPosition) => {
+    const R = 6371000; // 地球の半径（メートル）
+
+    const φ1 = toRad(initialCoords.lat);
+    const φ2 = toRad(markerPosition.lat);
+    const Δφ = toRad(markerPosition.lat - initialCoords.lat);
+    const Δλ = toRad(markerPosition.lng - initialCoords.lng);
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = R * c; // メートル単位で距離を計算
+
+    return distance;
+  };
+
+  return { distanceCalc };
+};
